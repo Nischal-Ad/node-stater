b9b91f724977a9c8101a38798b7bc616f254502c
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-05-22 21:50:43 +0545

    path alias error fix in build

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,7 @@ import ErrorHandler from '@Utils/errorHandler';
 export const TestUser = catchAsync(async (req, res, next) => {
   const user = ['dad', 'rams', 'shyam'];
 
-  if (user.includes('rams')) {
+  if (!user.includes('rams')) {
     return next(new ErrorHandler('user not found ', 404));
   }
   res.status(200).json({
